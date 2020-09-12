6de884898fafb65ecb250fc1174da73d8e1c101a
Adeyemi Olalekan Lawrence <olalekan.adeyemi@gmail.com>
2020-09-12 15:38:31 +0100

    Updated custom layout file for leaderBoards and then refactored the SectionsFragment

--- app/src/main/java/com/lakeside/gadsleaderboard/ui/main/SectionFragment.java
+++ app/src/main/java/com/lakeside/gadsleaderboard/ui/main/SectionFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.lakeside.gadsleaderboard.R;
 
@@ -21,7 +21,7 @@ public class SectionFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    private PageViewModel pageViewModel;
+    private LeaderBoardViewModel viewModel;
 
     public static SectionFragment newInstance(int index) {
         SectionFragment fragment = new SectionFragment();
@@ -34,12 +34,14 @@ public class SectionFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        //pageViewModel = ViewModelProviders.of(this).get(LeaderBoardViewModel.class);
+        viewModel = new ViewModelProvider(this).get(LeaderBoardViewModel.class);
+
         int index = 1;
         if (getArguments() != null) {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
-        pageViewModel.setIndex(index);
+        viewModel.setIndex(index);
     }
 
     @Override
@@ -47,11 +49,11 @@ public class SectionFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_leader_board, container, false);
-        final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //final TextView textView = root.findViewById(R.id.section_label);
+        viewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
